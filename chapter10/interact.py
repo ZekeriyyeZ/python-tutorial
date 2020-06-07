30881c2f4dadd02ef663266bd77d6b22dd79b940
ZekeriyyeZ <58814881+ZekeriyyeZ@users.noreply.github.com>
2020-06-07 21:44:46 +0400

    working with files and statements, expressions

---
+++ chapter10/interact.py
@@ -0,0 +1,63 @@
+'''
+while True:
+    reply = input('Enter text:') 
+    if reply == 'stop': break 
+    print(reply.upper())
+'''
+
+'''
+while True:
+    reply = input('Enter text:') 
+    if reply == 'stop': break 
+    print(int(reply) ** 2)
+print('Bye')
+'''
+
+'''
+while True:
+    reply = input('Enter text:') 
+    if reply == 'stop': 
+        break
+    elif not reply.isdigit(): 
+        print('Bad!' * 8)
+    else: 
+        print(int(reply) ** 2) 
+print('Bye')
+'''
+
+'''
+while True:
+    reply = input('Enter text:') 
+    if reply == 'stop': break 
+    try:
+        num = int(reply)
+    except:
+        print('Bad!' * 8)
+    else:
+        print(num ** 2)
+print('Bye')
+'''
+
+'''
+while True:
+    reply = input('Enter text:') 
+    if reply == 'stop': break 
+    try:
+        print(int(reply) ** 2)
+    except:
+        print('Bad!' * 8)
+print('Bye')
+'''
+
+while True:
+    reply = input('Enter text:') 
+    if reply == 'stop': break
+    elif not reply.isdigit(): 
+        print('Bad!' * 8)
+    else:
+        num = int(reply) 
+        if num < 20:
+            print('low') 
+        else: 
+            print(num ** 2) 
+print('Bye')
